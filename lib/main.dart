cb254ae30b965096f9c9d040c7a5c701f560ac50
MustufaMemon10 <mustufasam06@gmail.com>
2024-08-28 21:57:31 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -76,61 +76,9 @@ Future<void> checkNotificationPermission() async{
   else if (await Permission.notification.isDenied){
     final status = await Permission.notification.request();
     if(status.isGranted){
-
     }
     else{
-
     }
   }
 
-}
-Future<void> checkAndRequestExactAlarmPermission() async {
-  if (await Permission.scheduleExactAlarm.isGranted) {
-    log('Permission is already granted, you can schedule the notification');
-  } else if (await Permission.scheduleExactAlarm.isDenied) {
-    // Request the permission
-    final status = await Permission.scheduleExactAlarm.request();
-    if (status.isGranted) {
-      // Permission granted, you can schedule the notification
-    } else {
-      // Handle the case when permission is denied (show a message to the user)
-    }
-  }
-}
-Future<void> scheduleNotificationForTask(Task task) async {
-  await checkAndRequestExactAlarmPermission();
-  tz.initializeTimeZones();
-
-  final dueDate = task.dueDate.toUtc();
-  log('Original Due Date: $dueDate');
-
-  final location = tz.getLocation('Asia/Kolkata');
-
-  final scheduledDate = tz.TZDateTime.from(dueDate, tz.local);
-  log('Duedate utc : $dueDate');
-  log('Scheduled Date (IST): $scheduledDate');
-
-  if (scheduledDate.isBefore(tz.TZDateTime.now(location))) {
-    log('Scheduled date is in the past. Notification not scheduled.');
-    return;
-  }
-
-  await flutterLocalNotificationsPlugin.zonedSchedule(
-    0,
-    'Task Reminder',
-    'Your Task ${task.title} is due!',
-    tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
-    const NotificationDetails(
-      android: AndroidNotificationDetails(
-        'task_channel_id2',
-        'Task Notifications',
-        importance: Importance.max,
-        priority: Priority.high,
-      ),
-    ),
-    uiLocalNotificationDateInterpretation:
-    UILocalNotificationDateInterpretation.absoluteTime,
-  );
-
-  log('Notification scheduled for: $scheduledDate');
 }
--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,9 +1,62 @@
 import 'dart:developer';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest_all.dart' as tz;
 
 
 import '../main.dart';
 import '../models/task.dart';
 
 
+Future<void> checkAndRequestExactAlarmPermission() async {
+  if (await Permission.scheduleExactAlarm.isGranted) {
+    log('Permission is already granted, you can schedule the notification');
+  } else if (await Permission.scheduleExactAlarm.isDenied) {
+    // Request the permission
+    final status = await Permission.scheduleExactAlarm.request();
+    if (status.isGranted) {
+      // Permission granted, you can schedule the notification
+    } else {
+      // Handle the case when permission is denied (show a message to the user)
+    }
+  }
+}
+Future<void> scheduleNotificationForTask(Task task) async {
+  await checkAndRequestExactAlarmPermission();
+  tz.initializeTimeZones();
+
+  final dueDate = task.dueDate.toUtc();
+  log('Original Due Date: $dueDate');
+
+  final location = tz.getLocation('Asia/Kolkata');
+
+  final scheduledDate = tz.TZDateTime.from(dueDate, tz.local);
+  log('Duedate utc : $dueDate');
+  log('Scheduled Date (IST): $scheduledDate');
+
+  if (scheduledDate.isBefore(tz.TZDateTime.now(location))) {
+    log('Scheduled date is in the past. Notification not scheduled.');
+    return;
+  }
+
+  await flutterLocalNotificationsPlugin.zonedSchedule(
+    0,
+    'Task Reminder',
+    'Your Task ${task.title} is due!',
+    scheduledDate,
+    const NotificationDetails(
+      android: AndroidNotificationDetails(
+        'task_channel_id2',
+        'Task Notifications',
+        importance: Importance.max,
+        priority: Priority.high,
+      ),
+    ),
+    uiLocalNotificationDateInterpretation:
+    UILocalNotificationDateInterpretation.absoluteTime,
+  );
+
+  log('Notification scheduled for: $scheduledDate');
+}
